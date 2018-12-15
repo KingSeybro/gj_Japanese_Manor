5febf6a55dc137bf411ee199723808682abfbb77
Christian Zellot <christian@zellot.at>
2018-12-15 20:45:56 +0100

    server battle logic

--- app/src/gj_japanese_manor/scenes/overWorldScene.ts
+++ app/src/gj_japanese_manor/scenes/overWorldScene.ts
@@ -96,7 +96,7 @@ export class OverWorldScene extends BaseTileMapScene {
         Websocket.io.on(SharedConstants.EVENT_PLAYER_UPDATE, (p: PlayerInfo) => {
 
             if (p.id !== Websocket.io.id) {
-                console.log("player update" + JSON.stringify(p));
+                // console.log("player update" + JSON.stringify(p));
                 if (!self.otherPlayers.get(p.id)) {
                     let otherPlayer = this.physics.add.sprite(p.position.x, p.position.y, 'player');
                     otherPlayer.setDisplaySize(Constants.TILE_SIZE, Constants.TILE_SIZE)
