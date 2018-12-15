be58a313372b45af5db6dbfdc8341cbcde54e6c9
Christian Zellot <christian@zellot.at>
2018-12-15 21:56:08 +0100

    cont battlescene

--- app/src/gj_japanese_manor/websocket.ts
+++ app/src/gj_japanese_manor/websocket.ts
@@ -5,14 +5,14 @@ export class Websocket {
 
     public static io: SocketIOClient.Socket;
 
-    public static init(): void {
-    this.io = socketIo(Constants.SERVER_URL, {secure: true, path: '/klujam18server/socket.io/', transports: ['websocket'], upgrade: false});
+    public static init(): string {
+        this.io = socketIo(Constants.SERVER_URL, {secure: true, path: '/klujam18server/socket.io/', transports: ['websocket'], upgrade: false});
         this.io.connect();
 
         Websocket.io.on('message', function (p: any) {
             console.log('RECEIVE MESSAGE:', p);
-        })
-
+        });
+        return this.io.id;
     }
 
     public static isConnected(): boolean {
